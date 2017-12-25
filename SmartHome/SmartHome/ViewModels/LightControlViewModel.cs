17c4a7bb032ffe2d250177c2e0639e86a5d08a48
gmariott <giovanni.mariotta@fei.com>
2017-12-25 21:31:24 +0100

    Added RGB Text and converter XYBrightness to RGB and RGB to XYBrightness

--- SmartHome/SmartHome/ViewModels/LightControlViewModel.cs
+++ SmartHome/SmartHome/ViewModels/LightControlViewModel.cs
@@ -99,21 +99,47 @@ namespace SmartHome.ViewModels
         public string Name => parameter == null ? "Error Light Name" : parameter.Name;
 
 
-        private double redValue = 0;
-        public double RedValue
+        private byte redValue = 0;
+        private byte greenValue = 0;
+        private byte blueValue = 0;
+
+        public string RedValue
         {
             set
             {
-                redValue = value;
-                ApplyRed(value);
+                byte convertedValue = byte.Parse(value);
+                redValue = convertedValue;
+                ApplyRGB();
                 
             }
         }
 
-
-        private void ApplyRed(double value)
+        public string GreenValue
         {
-            
+            set
+            {
+                byte convertedValue = byte.Parse(value);
+                greenValue = convertedValue;
+                ApplyRGB();
+
+            }
+        }
+
+        public string BlueValue
+        {
+            set
+            {
+                byte convertedValue = byte.Parse(value);
+                blueValue = convertedValue;
+                ApplyRGB();
+
+            }
+        }
+
+
+        private void ApplyRGB()
+        {
+            RgbToXy(redValue, greenValue, blueValue);
         }
 
 
@@ -129,6 +155,71 @@ namespace SmartHome.ViewModels
             }
         }
 
+        public void  RgbToXy(byte r, byte g, byte b)
+        {
+            //if(parameter==null) return;
+
+            var red = PivotRgb(r / 255.0);
+            var green = PivotRgb(g / 255.0);
+            var blue = PivotRgb(b/ 255.0);
+
+            var x = red * 0.4124 + green * 0.3576 + blue * 0.1805;
+            var y = red * 0.2126 + green * 0.7152 + blue * 0.0722;
+            var bri = red * 0.0193 + green * 0.1192 + blue * 0.9505;
+
+            //parameter.ChangeBrightness((byte)bri);
+            //parameter.ColorCoordinates[0] = x;
+            //parameter.ColorCoordinates[1] = y;
+        }
+
+        private  double PivotRgb(double n)
+        {
+            return (n > 0.04045 ? Math.Pow((n + 0.055) / 1.055, 2.4) : n / 12.92) ;
+        }
+
+        private void XyBrightToRgb()
+        {
+            double x = parameter.ColorCoordinates[0];
+            double y = parameter.ColorCoordinates[1];
+            byte bri = parameter.Brightness;
+
+            var z = 1.0 - x - y;
+
+            var yLocal = bri / 255.0; // Brightness of lamp
+            var xLocal = (yLocal / y) * x;
+            var zLocal = (yLocal / y) * z;
+            var r = xLocal * 1.612 - yLocal * 0.203 - zLocal * 0.302;
+            var g = -xLocal * 0.509 + yLocal * 1.412 + zLocal * 0.066;
+            var b = xLocal * 0.026 - yLocal * 0.072 + zLocal * 0.962;
+            r = r <= 0.0031308 ? 12.92 * r : (1.0 + 0.055) * Math.Pow(r, (1.0 / 2.4)) - 0.055;
+            g = g <= 0.0031308 ? 12.92 * g : (1.0 + 0.055) * Math.Pow(g, (1.0 / 2.4)) - 0.055;
+            b = b <= 0.0031308 ? 12.92 * b : (1.0 + 0.055) * Math.Pow(b, (1.0 / 2.4)) - 0.055;
+            var maxValue = 0;
+            maxValue = Math.Max((int)r, (int)g);
+            maxValue = Math.Max( maxValue, (int)b);
+            r /= maxValue;
+            g /= maxValue;
+            b /= maxValue;
+
+            redValue = (byte)(Math.Round(r) * 255);
+            if (r < 0)
+            {
+                redValue = 255;    
+            }
+            greenValue = (byte)(Math.Round(g) * 255);
+            if (g < 0)
+            {
+                greenValue = 255;            
+            }
+
+            blueValue = (byte)(Math.Round(b) * 255);
+            if (b < 0)
+            {
+                blueValue = 255; 
+            }
+
+        }
+
         public double ColorCoordinateX
         {
             get
@@ -138,7 +229,7 @@ namespace SmartHome.ViewModels
             set
             {
                 Parameter?.ChangeColorCoordinates(value, ColorCoordinateY);
-                
+                XyBrightToRgb();
             }
         }
 
@@ -151,6 +242,7 @@ namespace SmartHome.ViewModels
             set
             {
                 Parameter?.ChangeColorCoordinates(ColorCoordinateX,value);
+                XyBrightToRgb();
             }
         }
 
